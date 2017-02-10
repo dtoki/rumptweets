93f8166a9fee4f5079d357f20fdb46049ede95f5
Dolapo Toki <dolapotoki@gmail.com>
2017-02-10 12:06:52 -0800

    changed access control to all

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var bucket = gcs.bucket('rumptweets-2c7cc.appspot.com');
 // Middleware to use for all requests
 router.use(function(req, res, next) {
     //Set the default headers needed
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:9090");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Content-Type","application/json");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
