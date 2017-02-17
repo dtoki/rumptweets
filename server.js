008203ebecde2e46cb0f859c35d42acb2ae83bd4
Dolapo Toki <dolapotoki@gmail.com>
2017-02-16 16:54:23 -0800

    changed folder upload location

--- server.js
+++ server.js
@@ -14,11 +14,16 @@ var app = express();
 app.use(bodyParser.json());
 app.use('/images',  express.static(__dirname + '/images'));
 
-// Storage for google bucket
-var gcs = storage({
+//Get the 3rd argument passed in
+var arg2= process.argv[2];
+var gcs;
+//If on dev machine auth using the passed in credentials
+(arg2=="-dev") ? gcs = storage({
     projectId: 'rumptweets-2c7cc',
     keyFilename: 'key/firebase_key.json'
-});
+}) : gcs = storage();
+
+//Connect to this bucket
 var bucket = gcs.bucket('rumptweets-2c7cc.appspot.com');
 
 // Load the html file
@@ -43,23 +48,52 @@ router.use(function(req, res, next) {
 // Serve the tweet
 app.get('/', function(req, res) {
     //res.send($.html());
-    res.send(facebookHtml.html());
+    //res.send(facebookHtml.html());
 });
 
+//Gentweet api
 router.route('/gentweet')
     .post(function(req, res) {
         var userPost;
         userPost = req.body;
         //Upload file
-        uploadToFirebaseStorage(userPost,function(data,publicUrl){
-            res.json({downloadUrl: data.mediaLink, publicUrl: publicUrl});
+        createUploadFolder((folderCreated)=>{
+            //Log if creating the folder was successful or not;
+            (folderCreated)? console.log("File system created folder"):console.log("File system could not create folder");
+            //Start upload process if the folder was created
+            (folderCreated)?uploadToFirebaseStorage(userPost, function(data,publicUrl,fileName){
+                res.json({downloadUrl: data.mediaLink, publicUrl: publicUrl});
+                //Delete file
+                fs.unlink(__dirname+fileName,(err)=>{
+                    (!err)?console.log("Successfully deleted file"):console.log("Encountered error while deleting file "+err);
+                });
+            }): res.sendStatus(500).json({message: "internal server error creating temp upload folder"});
         });
         
     });
+//Create folder
+function createUploadFolder(myCallback){
+    //Check if folder exists.. if it does not create it
+    fs.access(__dirname+"/image_upload_repo",fs.constants.F_OK,(err)=>{
+        if(err){
+            console.log("creating image_upload_repo folder...");
+            fs.mkdir(__dirname+"/image_upload_repo",(err)=>{   
+                myCallback((err==null)?true:false);
+            });
+        }else{
+            console.log("folder exsists");
+            myCallback(true);
+        };
+    });
+}
+//Upload to firebase
 function uploadToFirebaseStorage(userPost,myCallback){
-    // TODO create /image_upload_repo/ folder if it's not yet created.
     // Folder needs to created to stream
-     var file = fs.createWriteStream(__dirname+'/image_upload_repo/'+userPost.userId.toString()+'_and_'+userPost.imagePostId.toString() + '.png', {encoding: 'binary'});
+    try {
+        var file = fs.createWriteStream(__dirname+'/image_upload_repo/'+userPost.userId.toString()+'_and_'+userPost.imagePostId.toString() + '.png', {encoding: 'binary'});
+    } catch (error) {
+        console.log("Error creating file " +error);
+    }
     // Change words on tweet
     $('label.userInput').text(userPost.message.toString());
      var options = {
@@ -70,22 +104,34 @@ function uploadToFirebaseStorage(userPost,myCallback){
         windowSize:{ width: 1000
         , height: 768 },
     };
+    
     //Take the picture with the options above
     var renderStream = webshot($.html(),options);
+    
+    //Writhe the data into the file
     renderStream.on('data', function(data) {
         //Write data to file
-        file.write(data.toString('binary'), 'binary', function(err){
-            if(err){
-                console.log("error writing file to folder"+err);
-            }
-            console.log('writing file to folder...');
-        });
+        try {
+            file.write(data.toString('binary'), 'binary', function(err){
+                if(err){
+                    console.log("error writing file to folder"+err);
+                }
+                console.log('writing file to folder...');
+            });
+        } catch (error) {
+            console.log("error writing file"+ error);
+        }
+        
     });
-    //When the data ends then upload file
+    //When the data write ends ends then upload file
     renderStream.on('end', function(data){
-        //options for file
+        //Close the file write stream if not when you try to open again gigi
+        file.end();
+        //options to use to create the file
+        var folderLocation;
+        (arg2=="-dev")? folderLocation = "upload_folder_dev/" : folderLocation = "upload_folder/";
         var options = {
-            destination: 'server_upload_test/'+userPost.userId.toString()+'/'+userPost.imagePostId.toString()+'.png',
+            destination: folderLocation + userPost.userId.toString()+ '/' + userPost.imagePostId.toString() + '.png',
             public:true,
             validation: 'crc32c',
             metadata: {
@@ -108,8 +154,8 @@ function uploadToFirebaseStorage(userPost,myCallback){
                     //Log the self link useful to look at the meta data information.
                      //console.log(data[1]);
                     publicUrl = `https://storage.googleapis.com/${bucket.name}/${data[1].object}`;
-                    //Make callback when the file is set
-                    myCallback(file.metadata,publicUrl);
+                    //Make callback when the file is set withe the download url, public url and local path to the file
+                    myCallback(file.metadata,publicUrl,'/image_upload_repo/'+userPost.userId.toString()+'_and_'+userPost.imagePostId.toString() + '.png');
                    var metadata = {
                     metadata: {
                         'og:title':'Rump Tweets',
