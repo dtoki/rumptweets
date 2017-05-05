218b6bdb186152b6635e11bba5f8170b7c29530e
Dolapo Toki <dolapotoki@gmail.com>
2017-05-05 12:30:34 -0700

    fixed posturl, reconfig ci

--- app/server.js
+++ app/server.js
@@ -2,6 +2,8 @@
 const express = require("express"),
 app = express(),
 http = require("http"),
+https = require("https"),
+fs = require("fs"),
 bodyParser = require('body-parser');
 
 
@@ -11,10 +13,11 @@ var gcs;
 var bucket;
 
 //enable stack driver
-//require('@google-cloud/debug-agent').start({ allowExpressions: true,capture: { maxFrames: 20, maxProperties: 100 } });
+require('@google-cloud/debug-agent').start({ allowExpressions: true,capture: { maxFrames: 20, maxProperties: 100 } });
 
-
-//initGoogleServices();
+const certificate = fs.readFileSync("certs/cert.pem","utf8");
+const privateKey = fs.readFileSync("certs/private.pem","utf8");
+var credentials = { key: privateKey, cert: certificate}
 //define the static folders
 app.use(express.static(__dirname+'/front-end-module/build/default/'));
 
@@ -25,6 +28,9 @@ const apiroute = require('./image-server-module/api')
 app.use('/api', apiroute);
 
 
+https.createServer(credentials, app).listen(8443,()=>{
+    console.log(`Secure app listening on port 8443`)
+});
 
 http.createServer(app).listen(8080,()=>{
   console.log(`App listening on port 8080`);
